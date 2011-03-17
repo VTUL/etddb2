46d2a599fb5b228a8451a5b8f7bf19e825b075d4
Collin Brittle <cnbrittle@gmail.com>
2011-03-17 16:51:56 -0400

    Deleted, or renamed some files

--- db/migrate/20101202190735_add_etd_id_to_contents.rb
+++
@@ -1,9 +0,0 @@
-class AddEtdIdToContents < ActiveRecord::Migration
-  def self.up
-    add_column :contents, :etd_id, :integer
-  end
-
-  def self.down
-    remove_column :contents, :etd_id
-  end
-end
--- db/migrate/20101208193810_add_urn_into_etds.rb
+++
@@ -1,9 +0,0 @@
-class AddUrnIntoEtds < ActiveRecord::Migration
-  def self.up
-    add_column :etds, :urn, :string    
-  end
-
-  def self.down
-    remove_column :etds, :urn
-  end
-end
