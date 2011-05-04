94b32a88b93bf216040dd673ecfd72888c09a2e8
Sung Hee Park <shpark@vt.edu>
2011-05-04 17:29:53 -0400

    Commit all changes

--- app/models/content.rb
+++ app/models/content.rb
@@ -8,7 +8,8 @@ class Content < ActiveRecord::Base
   belongs_to :etd
   # validation
   validates :filename, :size, :availability, :types, :bound, :presence => true
-  validates :size, :numericality => {:greater_than_or_equal_to => 0} 
+  validates :size, :numericality => {:greater_than_or_equal_to => 0}
+  
  # validates_format_of :filename,
  #                     :with => %r{([A-Z]([A-Z]|[a-z])*)(_([a-z]|[A-Z])+)*(_[D|T])(_([0-9]{4}))\.(([a-z]|[A-Z]){3})},
  #                     :message => 'must have these component <Last name>_<first (and) middle initials>_T or D_<yyyy of defense' 
@@ -23,7 +24,7 @@ class Content < ActiveRecord::Base
     self.types = content_field.content_type.chomp
     #self.data = picture_field.read
     self.availability = 'unrestricted'
-    self.size = 10000
+    self.size = content_field.filesize
     self.bound = 'no'
   end
 
