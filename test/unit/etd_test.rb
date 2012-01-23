4df6cf3fedb3abcec6cb64fbb936997118da2a34
Collin Brittle <cnbrittle@gmail.com>
2012-01-23 15:54:54 -0500

    New completeness ETD test

--- test/unit/etd_test.rb
+++ test/unit/etd_test.rb
@@ -2,25 +2,29 @@ require 'test_helper'
 #module test_helper
 
 class EtdTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
   test "invalid with empty attributes" do
     etd = Etd.new
     assert !etd.valid?
     assert etd.errors[:abstract].any?
     assert etd.errors[:availability].any?
-    #assert etd.errors[:availability_description].any?
     assert etd.errors[:bound].any?
-    #assert etd.errors[:copyright_statement].any?
     assert etd.errors[:degree].any?
     assert etd.errors[:department].any?
     assert etd.errors[:dtype].any?
     assert etd.errors[:title].any?
-    #assert etd.errors[:adate].any?
-    #assert etd.errors[:cdate].any?
-    #assert etd.errors[:ddate].any?
-    #assert etd.errors[:rdate].any?
-    #assert etd.errors[:sdate].any?
     assert etd.errors[:url].any?
     assert etd.errors[:urn].any?
+  end
+
+  test "invalid without all required attributes" do
+    attrs = {:title => 'test', :abstract => 'abs', :bound => 'false', :urn => 'etd-1',
+             :url => 'http://etds.edu/etd-1/', :dtype => 'Thesis', :department => 'Art Theory',
+             :degree => 'Master of Theory', :availability => 'withheld'}
+
+    for key, value in attrs do
+      etd = Etd.new(attrs)
+      etd[key] = ''
+      assert !etd.valid?
+    end
   end
 end
