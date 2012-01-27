fef2647baa150f958fe49700b240f447f9e0b296
Collin Brittle <cnbrittle@gmail.com>
2012-01-27 16:49:18 -0500

    Pared down databases, and updated migrations to meet current needs.

--- db/schema.rb
+++ db/schema.rb
@@ -11,61 +11,50 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111012205750) do
+ActiveRecord::Schema.define(:version => 20120126205621) do
 
-  create_table "actions_roles_digital_objects", :id => false, :force => true do |t|
-    t.integer "action_id"
-    t.integer "role_id"
-    t.integer "digital_object_id"
-  end
-
-  add_index "actions_roles_digital_objects", ["action_id"], :name => "index_actions_roles_digital_objects_on_action_id"
-  add_index "actions_roles_digital_objects", ["digital_object_id"], :name => "index_actions_roles_digital_objects_on_digital_object_id"
-  add_index "actions_roles_digital_objects", ["role_id", "action_id", "digital_object_id"], :name => "index_actions_roles_digital_objects", :unique => true
-
-  create_table "availability_descriptions", :force => true do |t|
-    t.integer  "etd_id"
-    t.string   "availability"
+  create_table "availabilities", :force => true do |t|
+    t.string   "name"
     t.string   "description"
+    t.boolean  "retired"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "contents", :force => true do |t|
     t.integer  "etd_id"
+    t.integer  "availability_id"
     t.string   "uploaded_file_name"
     t.string   "uploaded_content_type"
     t.integer  "uploaded_file_size"
     t.datetime "uploaded_updated_at"
-    t.string   "availability"
     t.string   "bound"
     t.integer  "page_count"
-    t.datetime "timestamp"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.integer  "file_id"
     t.string   "file_type"
     t.integer  "duration"
     t.string   "dimensions"
-  end
-
-  create_table "copyright_statements", :force => true do |t|
-    t.integer  "etd_id"
-    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "degree_descriptions", :force => true do |t|
-    t.integer  "etd_id"
+  create_table "copyright_statements", :force => true do |t|
+    t.string   "description"
+    t.boolean  "retired"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "degrees", :force => true do |t|
     t.string   "name"
+    t.boolean  "retired"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "departments", :force => true do |t|
-    t.integer  "etd_id"
     t.string   "name"
+    t.boolean  "retired"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -76,49 +65,30 @@ ActiveRecord::Schema.define(:version => 20111012205750) do
     t.datetime "updated_at"
   end
 
-  create_table "doc_type_descriptions", :force => true do |t|
-    t.integer  "etd_id"
+  create_table "document_types", :force => true do |t|
     t.string   "name"
+    t.boolean  "retired"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "etds", :force => true do |t|
-    t.string   "urn"
-    t.string   "degree"
-    t.string   "department"
-    t.string   "dtype"
     t.text     "title"
     t.text     "abstract"
-    t.string   "availability"
-    t.text     "availability_description"
-    t.text     "copyright_statement"
+    t.string   "bound"
+    t.string   "status"
+    t.string   "urn"
+    t.string   "url"
     t.date     "ddate"
     t.date     "sdate"
     t.date     "adate"
     t.date     "cdate"
     t.date     "rdate"
-    t.string   "pid"
-    t.string   "url"
-    t.datetime "timestamp"
-    t.string   "bound"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "committee_chair_id"
-    t.integer  "author_id"
-  end
-
-  create_table "etds_people", :id => false, :force => true do |t|
-    t.integer "etd_id"
-    t.integer "person_id"
-  end
-
-  add_index "etds_people", ["etd_id", "person_id"], :name => "index_etds_people_on_etd_id_and_person_id", :unique => true
-  add_index "etds_people", ["person_id"], :name => "index_etds_people_on_person_id"
-
-  create_table "keywords", :force => true do |t|
-    t.integer  "etd_id"
-    t.string   "word"
+    t.integer  "availability_id"
+    t.integer  "copyright_statement_id"
+    t.integer  "department_id"
+    t.integer  "document_type_id"
+    t.integer  "privacy_statement_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -131,7 +101,6 @@ ActiveRecord::Schema.define(:version => 20111012205750) do
     t.string   "suffix"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "roles_mask"
     t.string   "email",                                 :default => "", :null => false
     t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
     t.string   "reset_password_token"
@@ -159,11 +128,19 @@ ActiveRecord::Schema.define(:version => 20111012205750) do
   add_index "people_roles", ["role_id"], :name => "index_people_roles_on_role_id"
 
   create_table "permissions", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.integer  "user_action_id"
     t.integer  "role_id"
     t.integer  "digital_object_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "privacy_statements", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.boolean  "retired"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "provenances", :force => true do |t|
@@ -176,22 +153,6 @@ ActiveRecord::Schema.define(:version => 20111012205750) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "sessions", :force => true do |t|
-    t.string   "session_id", :null => false
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
-  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
-
-  create_table "urns", :force => true do |t|
-    t.string   "urn"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
