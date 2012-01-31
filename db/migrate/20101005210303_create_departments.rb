96e4d5ef1cb8f940d83fcf5ed85c6c7efc712980
Collin Brittle <cnbrittle@gmail.com>
2012-01-31 15:54:34 -0500

    Aloow multiple departments per etd

--- db/migrate/20101005210303_create_departments.rb
+++ db/migrate/20101005210303_create_departments.rb
@@ -6,9 +6,15 @@ class CreateDepartments < ActiveRecord::Migration
 
       t.timestamps
     end
+
+    create_table :etds_departments
+      t.integer :etd_id
+      t.integer :department_id
+    end
   end
 
   def self.down
     drop_table :departments
+    #drop_table :etds_departments
   end
 end
