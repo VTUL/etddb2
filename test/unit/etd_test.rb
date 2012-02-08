53720fb32ba62a0b9ff43cebb992058f6e8f8663
Sung Hee Park <shpark@vt.edu>
2012-02-08 17:14:28 -0500

    Etd unit test

--- test/unit/etd_test.rb
+++ test/unit/etd_test.rb
@@ -8,15 +8,9 @@ class EtdTest < ActiveSupport::TestCase
     assert etd.errors[:abstract].any?
     assert etd.errors[:availability_id].any?
     assert etd.errors[:bound].any?
-<<<<<<< HEAD
-    #assert etd.errors[:degree].any?
-    assert etd.errors[:department].any?
-    assert etd.errors[:dtype].any?
-=======
     assert etd.errors[:copyright_statement_id].any?
     assert etd.errors[:degree_id].any?
     assert etd.errors[:document_type_id].any?
->>>>>>> 4fce0655f41200f9952f807e5a21314182331c05
     assert etd.errors[:title].any?
     assert etd.errors[:privacy_statement_id].any?
     assert etd.errors[:url].any?
@@ -24,21 +18,9 @@ class EtdTest < ActiveSupport::TestCase
   end
 
   test "invalid without all required attributes" do
-<<<<<<< HEAD
-    attrs = {:title => 'test', :abstract => 'abs', :bound => 'false', :urn => 'etd-1',
-             :url => 'http://etds.edu/etd-1/', :dtype => 'Thesis', :department => 'Art Theory',
-             #:degree => 'Master of Theory',
-             :availability => 'withheld'}
-=======
     attrs = {:abstract => 'abs', :availability_id => 1, :bound => false,
              :copyright_statement_id => 1, :degree_id => 1, :document_type_id => 1,
-<<<<<<< HEAD
-             :title => 'test', :url => 'http://etds.edu/etd-1/', :urn => 'etd-1'}
->>>>>>> 4fce0655f41200f9952f807e5a21314182331c05
-=======
              :title => 'title', :privacy_statement_id => 1, :url => "/", :urn => 0}
->>>>>>> 067914d8468f0a47ea1bfea97c7213599ebb80e0
-
     for key, value in attrs do
       etd = Etd.new(attrs)
       etd[key] = nil
