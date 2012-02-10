9da308018742c35335d351c883b509889d74614a
Collin Brittle <cnbrittle@gmail.com>
2012-02-10 14:26:35 -0500

    Devise fields are attr_accessible.

--- app/models/person.rb
+++ app/models/person.rb
@@ -19,11 +19,10 @@ class Person < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [ :pid ]
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [ :pid ]
 
   # Setup accessible (or protected) attributes for your model
-  #attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me
 
   # for authentication through matching the login name to the stored names.
   def self.find_by_pid(pid)
