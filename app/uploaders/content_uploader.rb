3509880929ddcf2a8060f6e83d9429a7dbe0c9d3
Collin Brittle <cnbrittle@gmail.com>
2012-03-29 16:46:30 -0400

    Content is now saved to a sensible loaction

--- app/uploaders/content_uploader.rb
+++ app/uploaders/content_uploader.rb
@@ -17,7 +17,7 @@ class ContentUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     #"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    "bin/submitted"
+    "bin/submitted/#{model.etd.urn}/"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
