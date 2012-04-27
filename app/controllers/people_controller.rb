1543dee28029ea8a723422399f6788ef3ae6059e
Collin Brittle <rotated8@vt.edu>
2012-04-27 14:17:48 -0400

    Whitelisted origins, and fixed a header link.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -23,84 +23,6 @@ class PeopleController < ApplicationController
     end
   end
 
-  # The functions below are relics from before Devise handled authentication.
-  # They are left in for completeness, and laziness.
-
-  # GET /people/new
-  # GET /people/new.xml
-  #def new
-  #  @person = Person.new
-  #  @ability = Ability.new(@person)
-  #
-  #  respond_to do |format|
-  #    format.html # new.html.erb
-  #    format.xml  { render :xml => @person }
-  #  end
-  #end
-  #
-  # POST /people
-  # POST /people.xml
-  #def create
-  #  @person = Person.new(params[:person])
-  #  @person.display_name = @person.first_name + " " + @person.last_name
-  #
-  #  respond_to do |format|
-  #    if @person.save
-  #      format.html { redirect_to(@person, :notice => 'Person was successfully created.') }
-  #      format.xml  { render :xml => @person, :status => :created, :location => @person }
-  #    else
-  #      format.html { render :action => "new" }
-  #      format.xml  { render :xml => @person.errors, :status => :unprocessable_entity }
-  #    end
-  #  end
-  #end
-  #
-  # GET /people/1/edit
-  #def edit
-  #  @person = Person.find(params[:id])
-  #  @ability = Ability.new(@person)
-  #end
-  #
-  # PUT /people/1
-  # PUT /people/1.xml
-  #def update
-  #  authorize! :assign_roles, @person if params[:person][:roles]
-  #
-  #  @person = Person.find(params[:id])
-  #
-  #  respond_to do |format|
-  #    if @person.update_attributes(params[:person])
-  #      format.html { redirect_to(@person, :notice => 'Person was successfully updated.') }
-  #      format.xml  { head :ok }
-  #    else
-  #      format.html { render :action => "edit" }
-  #      format.xml  { render :xml => @person.errors, :status => :unprocessable_entity }
-  #    end
-  #  end
-  #end
-  #
-  # DELETE /people/1
-  # DELETE /people/1.xml
-  #def destroy
-  #  @person = Person.find(params[:id])
-  #
-  #  # Destroy ETDs this person has Authored.
-  #  author = Role.where(:name => "Author").first()
-  #  for pr in @person.people_roles do
-  #    if pr.role_id == author.id
-  #      etd = Etd.find(pr.etd_id)
-  #      etd.destroy
-  #    end
-  #  end
-  #
-  #  @person.destroy
-  #
-  #  respond_to do |format|
-  #    format.html { redirect_to(people_url) }
-  #    format.xml  { head :ok }
-  #  end
-  #end
-
   # POST /people/find/
   def find
     respond_to do |format|
@@ -126,6 +48,12 @@ class PeopleController < ApplicationController
     pr.etd_id = params[:etd_id]
     pr.role_id = Role.where(:name => params[:committee_type]).first().id
     Person.find(params[:committee]).people_roles << pr
+
+    # Whitelist params[:origin]
+    origins = ["/etds/", "/next_new/"]
+    if !origins.include?(params[:origin])
+      params[:origin] = "/etds/"
+    end
 
     respond_to do |format|
       format.html{ redirect_to(params[:origin] + params[:etd_id]) }
