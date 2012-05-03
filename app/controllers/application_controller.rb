d46cc1c59d8b47282deb5385cf81f7cbf5c5ca70
Collin Brittle <rotated8@vt.edu>
2012-05-03 17:30:22 -0400

    redirect to the person's homepage on login.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   # Overwriting the login redirect.
   def after_sign_in_path_for(resource_or_scope)
-    etds_path
+    person_path(current_person)
   end
 
 end
