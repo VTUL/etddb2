f7b7caf88fde60314eeef4dea4aca58bc5fbfd79
Collin Brittle <rotated8@vt.edu>
2012-06-01 14:45:56 -0400

    Added Email Pref

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120530204657) do
+ActiveRecord::Schema.define(:version => 20120601173104) do
 
   create_table "availabilities", :force => true do |t|
     t.string   "name"
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(:version => 20120530204657) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.boolean  "show_email"
   end
 
   add_index "people", ["email"], :name => "index_people_on_email", :unique => true
