afbfd2b97b32e4fc2f72892e720153c254ec5a41
Collin Brittle <rotated8@vt.edu>
2012-06-22 19:23:10 -0400

    Got rid of the ETD helper methods and fixed some whitespace.

--- app/helpers/etds_helper.rb
+++ app/helpers/etds_helper.rb
@@ -1,17 +1,2 @@
 module EtdsHelper
-  def get_creators(etd)
-    if block_given?
-      yield Person.where(id: etd.people_roles.where(role_id: Role.where(group: 'Creators')).pluck(:person_id)).order('last_name ASC')
-    else
-      Person.where(id: etd.people_roles.where(role_id: Role.where(group: 'Creators')).pluck(:person_id)).order('last_name ASC')
-    end
-  end
-
-  def get_collabs(etd)
-    if block_given?
-      yield Person.find(etd.people_roles.where(role_id: Role.where(group: 'Collaborators')).pluck(:person_id))
-    else
-      Person.find(etd.people_roles.where(role_id: Role.where(group: 'Collaborators')).pluck(:person_id))
-    end
-  end
 end
