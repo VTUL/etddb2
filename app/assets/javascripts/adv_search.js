a7a9ed575444e947cec61f0b6a4d7885468d1f81
CJ Gonzalez <cjg90@vt.edu>
2012-09-12 10:41:37 -0400

    Very early stage of search, changing result # pref crashes

---
+++ app/assets/javascripts/adv_search.js
@@ -0,0 +1,3 @@
+var field = $('<input id="business_loc" multiple="multiple" name="search_using" type="hidden" style="color: rgb(175, 175, 175); " value="13.0454044">');
+var form = $('page_pref_form');
+form.append(field);
