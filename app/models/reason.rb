7ce8511a61a614f9b2c1e80bfdcb78fb89b4814b
Collin Brittle <rotated8@vt.edu>
2012-10-29 19:18:24 -0400

    Fix error where warn_before_approval would not accept 'false'.

--- app/models/reason.rb
+++ app/models/reason.rb
@@ -4,7 +4,7 @@ class Reason < ActiveRecord::Base
   has_many :provenances, as: :model
   has_many :conversations, as: :model
 
-  validates_presence_of :name, :months_to_release, :months_to_warning, :warn_before_approval
+  validates_presence_of :name, :months_to_release, :months_to_warning
   validates :months_to_release, :months_to_warning, numericality: { only_integer: true }
   validates :warn_before_approval, inclusion: {in: [true, false], message: "must be boolean"}
 end
