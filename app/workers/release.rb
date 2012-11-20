41118f6f443a4345c11c732a5405b999ce5543d4
Collin Brittle <rotated8@vt.edu>
2012-11-20 10:22:15 -0500

    Add mailers and provenances to workers.

--- app/workers/release.rb
+++ app/workers/release.rb
@@ -3,8 +3,11 @@ class Release
   
   def self.perform(class_type, class_id)
     klass = class_type.constantize.find(class_id)
-
     klass.availability = klass.availability.release_availability
     klass.save
+
+    Provenance.create(person: Person.where(group: 'Administration').first, action: 'automatically released', model: klass)
+
+    EtddbMailer.release_authors(klass).deliver
   end
 end
--- app/workers/warning.rb
+++ app/workers/warning.rb
@@ -4,6 +4,8 @@ class Warning
   def self.perform(class_type, class_id)
     klass = class_type.constantize.find(class_id)
 
-    # TODO: Warn.
+    EtddbMailer.warn_authors(klass).deliver
+
+    Provenance.create(person: Person.where(group: 'Administration').first, action: 'automatically warned the authors of', model: klass)
   end
 end
