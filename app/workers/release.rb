78ea17315ba3b0b72b0f4369240de1d055e813df
Collin Brittle <rotated8@vt.edu>
2012-11-20 13:32:23 -0500

    Releasing an ETD should change it's status.

--- app/workers/release.rb
+++ app/workers/release.rb
@@ -4,6 +4,7 @@ class Release
   def self.perform(class_type, class_id)
     klass = class_type.constantize.find(class_id)
     klass.availability = klass.availability.release_availability
+    klass.status = 'Released' if klass.class.name == 'Etd'
     klass.save
 
     Provenance.create(person: Person.where(group: 'Administration').first, action: 'automatically released', model: klass)
