cbe67c7974dc9effdd8439f03b5f1a3474801216
CJ Gonzalez <cjg90@vt.edu>
2013-03-27 11:01:28 -0400

    Added pagination for profile page

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -30,10 +30,11 @@ class PeopleController < ApplicationController
     # Previous query
     #@my_etds = Etd.find(@person.people_roles.where(role_id: Role.where(group: 'Creators')).pluck(:etd_id))
     # New query to take advantage of pagination
-    @my_etds = Etd.paginate(page: params[:page], per_page: @per_page, include: [:people_roles],
+    @my_etds = Etd.paginate(page: params[:page_my_etds], per_page: @per_page, include: [:people_roles],
                             conditions: {"people_roles.person_id" => params[:id], "people_roles.role_id" => Role.where(group: 'Creators')})
-    @committee_etds = Etd.find(@person.people_roles.where(role_id: Role.where(group: 'Collaborators')).pluck(:etd_id))
-    @reviewable_etds = Etd.where(status: "Submitted")
+    @committee_etds = Etd.paginate(page: params[:page_committee], per_page: @per_page, include: [:people_roles],
+                            conditions: {"people_roles.person_id" => params[:id], "people_roles.role_id" => Role.where(group: 'Collaborators')})
+    @reviewable_etds = Etd.where(status: "Submitted").paginate(:page => params[:page_reviews], per_page: @per_page)
 
     respond_to do |format|
       format.html # show.html.erb
