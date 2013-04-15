c2fd615994d9ad067c51b587b460120ab2da302c
CJ Gonzalez <cjg90@vt.edu>
2013-04-15 10:48:30 -0400

    Facets now display by default if the browser has no javascript

--- app/assets/javascripts/facets.js
+++ app/assets/javascripts/facets.js
@@ -1,10 +1,19 @@
 $(document).ready(function () {
-  $('.facet_title').collapser({
-      target: 'next',
-      targetOnly: 'ul',
-      effect: 'slide',
-      changeText: 0,
-      expandClass: 'expand',
-      collapseClass:'collapse'
+    /*
+     * For browsers WITH javascript minimize facets, otherwise they will
+     *  all be displayed
+     */
+    $facets = $(".minified");
+    $facets.each(function() {
+        $(this).css('display', 'none');
+    });
+
+    $('.facet_title').collapser({
+        target: 'next',
+        targetOnly: 'ul',
+        effect: 'slide',
+        changeText: 0,
+        expandClass: 'expand',
+        collapseClass: 'collapse'
     });
 });
