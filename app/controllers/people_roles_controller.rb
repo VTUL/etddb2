f4d125953046479c5b321734ae1b2addc7235233
Collin Brittle <cnbrittle@gmail.com>
2013-04-23 17:44:05 -0400

    Non-admins need access to create people_roles.
    Otherwise you couldn't claim ETDs for review.

--- app/controllers/people_roles_controller.rb
+++ app/controllers/people_roles_controller.rb
@@ -1,6 +1,6 @@
 class PeopleRolesController < ApplicationController
   respond_to :html, :json, :js
-  before_filter :admin_only
+  before_filter :admin_only, except: [:create]
 
   # GET /people_roles
   # GET /people_roles.xml
