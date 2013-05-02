0c99f05261a2d1452024d30a3dfc4ef92aa32402
Collin Brittle <cnbrittle@gmail.com>
2013-05-02 18:22:36 -0400

    'or' is DANGEROUS. Use '||' instead.

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -41,7 +41,7 @@ class ContentsController < ApplicationController
     @etd = @content.etd
 
     # TODO: Should anyone associated with the ETD have unfettered access, or just the creators and collaborators?
-    if Etd::ACCESS.matches?(request.ip, @etd.availability, @content.availability) or @etd.people.include?(current_person)
+    if Etd::ACCESS.matches?(request.ip, @etd.availability, @content.availability) || @etd.people.include?(current_person)
       send_file(@content.content.path, filename: @content.content_file_name, type: @content.content_content_type)
     else
       # TODO: Perhaps log IPs that are hitting these pages?
